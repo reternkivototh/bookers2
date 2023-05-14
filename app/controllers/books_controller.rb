9a72deebdbd1874ef6abcc1ad206e3926bc68b19
ReturnKivototh <126948762+reternkivototh@users.noreply.github.com>
2023-05-14 12:49:11 +0000

    [Add] edit user_profile

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,12 +7,27 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_detail_path
+    redirect_to user_path(@book.user_id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
   end
 end
