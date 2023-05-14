43e18378e51c08b01053e35af1632513f3ac0406
ReturnKivototh <126948762+reternkivototh@users.noreply.github.com>
2023-05-14 15:16:01 +0000

    [Add] Link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
 
   def edit
     @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get 'about' => 'homes#about', as: 'about'
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:new, :create, :destroy, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
