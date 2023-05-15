908dbf866743126fc4cf54a20b49c4438ecb66e9
ReturnKivototh <126948762+reternkivototh@users.noreply.github.com>
2023-05-15 14:20:46 +0000

    book edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,16 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(list_params)
+    redirect_to book_path(book.id)
+  end
+
   def show
     @book = Book.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'about' => 'homes#about', as: 'about'
   devise_for :users
   resources :users, only: [:show, :index, :edit, :update]
-  resources :books, only: [:new, :create, :destroy, :index, :show]
+  resources :books, only: [:new, :create, :edit, :destroy, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
