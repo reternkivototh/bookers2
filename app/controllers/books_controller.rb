32c5b4f04b8253348370b01ade78b28557f74826
ReturnKivototh <126948762+reternkivototh@users.noreply.github.com>
2023-05-17 12:49:07 +0000

    [Add] validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.user_id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -26,8 +29,8 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(list_params)
-    redirect_to book_path(book.id)
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def show
@@ -38,6 +41,6 @@ class BooksController < ApplicationController
 
 
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
   end
 end
