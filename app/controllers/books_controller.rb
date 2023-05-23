2deda2243a4f002299aeb814bde0acfe1563ab4c
ReturnKivototh <126948762+reternkivototh@users.noreply.github.com>
2023-05-23 14:27:01 +0000

    [Add] 部分テンプレート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,8 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def index
     @users = User.all
+    @user = current_user
+    @book = Book.new
   end
 
   def edit
